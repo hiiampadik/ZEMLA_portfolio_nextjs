fd6493e20aa7d36273845daa8663688039aee50a
hiiampadik <brona.musil@gmail.com>
2022-12-29 11:34:08 +0100

    Layout

--- components/Layout.js
+++ components/Layout.js
@@ -7,32 +7,14 @@ import React, { useState, useEffect } from "react";
 
 import styles from "../styles/Layout.module.scss";
 
-import { propTypes } from "@sanity/block-content-to-react";
 import { useTheme } from "next-themes";
 
 export default function Layout({ children }) {
   const { theme, setTheme } = useTheme();
-  const [showPage, setShowPage] = useState(true);
-  const [showSvatba, setShowSvatba] = useState(false);
 
   const [mounted, setMounted] = useState(false);
   useEffect(() => {
     setMounted(true);
-  }, []);
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      if (
-        window.location.pathname == "/cs/svatba" ||
-        window.location.pathname == "/en/svatba" ||
-        window.location.pathname == "/svatba"
-      ) {
-        setShowSvatba(true);
-      }
-      if (window.location.host === "www.zem.la") {
-        setShowPage(false);
-      }
-    }
   }, []);
 
   const handleQuality = (newTheme) => {
@@ -61,14 +43,7 @@ export default function Layout({ children }) {
   return (
     <div>
       <Header />
-
-      {showSvatba ? (
-        <main className={styles.main}>{children}</main>
-      ) : (
-        <>
-            {getContent()}
-        </>
-      )}
+      {getContent()}
     </div>
   );
 }
